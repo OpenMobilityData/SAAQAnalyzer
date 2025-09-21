1af3b5ae213494a7dbdb38ec056ead3d02c0b578
rhoge <rick.hoge@mcgill.ca>
2025-09-21 13:21:07 -0400

    Initial Commit

---
+++ SAAQAnalyzer/SAAQAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAAQAnalyzerApp.swift
+//  SAAQAnalyzer
+//
+//  Created by Rick Hoge on 2025-09-21.
+//
+
+import SwiftUI
+
+@main
+struct SAAQAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
